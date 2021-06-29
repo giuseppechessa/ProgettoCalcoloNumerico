e264e7d0d305f271cd3f3f2b8fe88b51fa088714
GppChss <giuseppe.chessa@mail.polimi.it>
2021-06-29 22:05:12 +0200

    reworkato un po' le funzioni

--- CalcoloScript.m
+++ CalcoloScript.m
@@ -1,3 +1,6 @@
+Tfine=1000;
+passo=0.1;
+
 b=0.055;
 sigma=0.005;
 g=0.04;
@@ -8,9 +11,9 @@ E0=5000;
 I0=0;
 R0=0;
 
-h=0:0.1:100;
+h=0:passo:Tfine;
 SIERModel=@(t,y)[(-b/N)*y(1)*y(2);sigma*y(3)-g*y(2);(b/N)*y(1)*y(2)-sigma*y(3);g*y(2)];
-[t,y_eul]=cranknic(SIERModel,[0 1000],[S0;I0;E0;R0],1000);
+[t,y_eul]=cranknic(SIERModel,[S0;I0;E0;R0],h);
 
 plot(t,y_eul);
 legend({'S','I','E','R'},'Location','southwest')
--- cranknic.m
+++ cranknic.m
@@ -1,13 +1,13 @@
-function [t,u]= cranknic(fun ,tspan ,y0 ,Nh)
+function [t,u]= cranknic(fun,y0,t)
 tol =1.e-8;
 kmax =200;
-h2=( tspan(2)-tspan (1))/(2*Nh); 
-t=linspace(tspan(1),tspan(2),Nh+1)';
+h2=( t(2)-t(1))/(2); 
 d=numel(y0);
-u=zeros(Nh+1,d);
+num=numel(t);
+u=zeros(num,d);
 B0=eye(d); % matrice iniziale per innestare Broyden
 u(1 ,:)=y0; % trasposta anche di variabili complesse
-for n=1:Nh
+for n=1:num-1
     F=@(x)x-u(n,:)' -h2*( fun(t(n),u(n,:)')+ fun(t(n+1),x));
     u(n+1 ,:)=(broyden(F,B0 ,u(n,:)' ,tol ,kmax))';
 end
